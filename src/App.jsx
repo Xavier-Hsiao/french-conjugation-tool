052598335879ddfe676cbb6973baeec9f1b24973
Xavier-Hsiao <tim88776@gmail.com>
2024-01-05 15:07:14 +0800

    Init: initialize project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import styles from "./App.module.scss";
+import * as FrenchVerbs from "french-verbs";
+import Lefff from "french-verbs-lefff/dist/conjugations.json";
+
+export default function App() {
+  return (
+    <div className={styles.container}>
+      {/* <Header />
+      <Search />
+      <Main />
+      <Footer /> */}
+    </div>
+  );
+}
+
+function Header() {
+  
+}
