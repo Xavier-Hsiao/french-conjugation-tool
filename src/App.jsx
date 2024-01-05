71ac096f6938ad9587f7c8fa2b15d731f52dc7db
Xavier-Hsiao <tim88776@gmail.com>
2024-01-05 17:03:05 +0800

    build: add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,59 @@
 import styles from "./App.module.scss";
 import * as FrenchVerbs from "french-verbs";
 import Lefff from "french-verbs-lefff/dist/conjugations.json";
+import Header from "./components/Header/Header";
 
 export default function App() {
   return (
     <div className={styles.container}>
-      {/* <Header />
+      <Header />
       <Search />
       <Main />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
 
-function Header() {
-  
+function Search() {
+  return (
+    <div className={styles.search}>
+      <input type="text" className={styles.searchInput} />
+      <img src="src/assets/images/icon-search.svg" alt="search-icon" />
+    </div>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <h1>Aller</h1>
+      <div className={styles.divider}>
+        <p>Conjugaison</p>
+      </div>
+      <div className={styles.result}>
+        <p>Je vais</p>
+        <p>Tu vas</p>
+        <p>Il va</p>
+        <p>Elle va</p>
+        <p>Nous allons</p>
+        <p>Vous allez</p>
+        <p>Ils vont</p>
+      </div>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <div className="source">
+        <span>Source</span>
+        <a href="#">https://en.wikitionary.org/wiki/blah</a>
+        <img
+          src="src/assets/images/icon-new-window.svg"
+          alt="new-window-icon"
+        />
+      </div>
+    </footer>
+  );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,46 @@
+import styles from "./Header.module.scss";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.imageWrapper}>
+        <img
+          src="src/assets/images/logo.svg"
+          alt="logo"
+          className={styles.logo}
+        />
+      </div>
+      <nav className={styles.navbar}>
+        <div className={styles.tenseGroup}>
+          <select>
+            <option value="present">Présent</option>
+            <option value="futur">Futur</option>
+            <option value="passe-simple">Passé simple</option>
+            <option value="passe-compose">Passé composé</option>
+            <option value="plus-que-parfait">Plus que parfait</option>
+            <option value="imparfait">Imparfait</option>
+            <option value="subjonctif-present">Subjonctif présent</option>
+          </select>
+          <img
+            src="src/assets/images/icon-arrow-down.svg"
+            alt="selection-down"
+            className={styles.down}
+          />
+        </div>
+        <div className={styles.modeGroup}>
+          <label className={styles.switch}>
+            <input type="checkbox" />
+            <span className={`${styles.slider} ${styles.round}`}></span>
+          </label>
+          <div>
+            <img
+              src="src/assets/images/icon-moon.svg"
+              alt="dark-mode-icon"
+              className={styles.moon}
+            />
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
